833192ffa4add3fae0adf99856a356fa7e57d512
anarchyreloaded <roman.koschar@gmail.com>
2022-11-09 19:19:59 +0100

    Add files via upload

---
+++ Uptime Calculator/src/main/java/at/fhtw/bic/uptimecalculatorbic/controller/src/main/java/at/fhtw/bic/uptimecalculatorbic/controller/UptimeController.java
@@ -0,0 +1,23 @@
+package at.fhtw.bic.uptimecalculatorbic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UptimeController {
+    @GetMapping("/uptime/minutes")
+    public double calculateUptimeMinutes(@RequestParam double relative) {
+        double minutesOfMonth = 60*24*30;
+        double definedUptimeInPercent = relative / 100;
+        return minutesOfMonth * definedUptimeInPercent; // uptime in minutes
+    }
+
+    @GetMapping("/uptime/hours")
+    public double calculateUptimeHours(@RequestParam double relative) {
+        double hoursOfMonth = 24*30;
+        double definedUptimeInPercent = relative / 100;
+        return hoursOfMonth * definedUptimeInPercent;   // uptime in hours
+    }
+
+}
